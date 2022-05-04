94d2307831e5895c828680dcea0812165362fead
Oleksandr Verba <101819021+OleksandrVerba@users.noreply.github.com>
2022-05-04 10:51:58 +0300

    Update hw_01_calculator.py

--- hw_01_calculator.py
+++ hw_01_calculator.py
@@ -1,22 +1,31 @@
-try:
-    input_case = int(input('Please, choose an operand by number:\n1. +\n2. -\n3. *\n4. /'
-                           '\nYour choice [1-4]: '))
-
-    value_0 = float(input('\nPlease, type a first number.\n'))
-    value_1 = float(input("\nNow, type a second:\n"))
-
-    if input_case == 1:
-        print('\nCalculation result is:', value_0 + value_1)
-    elif input_case == 2:
-        print('\nCalculation result is:', value_0 - value_1)
-    elif input_case == 3:
-        print('\nCalculation result is:', value_0 * value_1)
-    elif input_case == 4:
-        print('\nCalculation result is:', value_0 / value_1)
-    else:
-        print('\nSorry, wrong operand number. Try again.')
-
-except ZeroDivisionError:
-    print("\nSorry, but you can't divide by zero.")
-except ValueError:
-    print('\nOops! It was definitely wrong key. Please, try again.')
+key_letter = input('Greetings!\nFor using me press any key.\nFor quit press "q".\n')
+if key_letter not in "Q, q":
+    while key_letter not in "Q, q":
+        try:
+            input_case = input('\nPlease, choose an operand by number:\n1. +\n2. -\n3. *\n4. /'
+                               '\nYour choice [1-4]: ')
+            if input_case in "1234":
+                value_0 = float(input('\nPlease, type a first number.\n'))
+                value_1 = float(input("\nNow, type a second:\n"))
+                result = float()
+                if input_case in '1':
+                    result = value_0 + value_1
+                elif input_case in '2':
+                    result = value_0 - value_1
+                elif input_case in '3':
+                    result = value_0 * value_1
+                elif input_case in '4':
+                    result = value_0 / value_1
+                print('\nCalculation result is:', result)
+            elif input_case not in "1234":
+                print("\nSorry, but it's a wrong operand. Try again?")
+            key_letter = input('\nPress any key for repeat or "q" for quit.\n')
+        except ZeroDivisionError:
+            print("\nSorry, but you can't divide by zero. Let's try again?")
+            key_letter = input('\nPress any key for repeat or "q" for quit.\n')
+        except ValueError:
+            print('\nOops! It was definitely wrong key. Go try again?')
+            key_letter = input('\nPress any key for repeat or "q" for quit.\n')
+    print('\nHave a nice day!')
+else:
+    print('\nHave a nice day!')
